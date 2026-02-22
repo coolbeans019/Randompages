b4d8343ee87f7404a0c221ef3693718d1475c0f9
coolbeans <hkaleds3s@gmail.com>
2026-02-22 14:20:50 +0000

    Refactor choice list and link assignments
    Updated the choice list and modified link assignments based on new choices.

--- choicemaker.js
+++ choicemaker.js
@@ -1,19 +1,17 @@
-const list = ["khaled's aztec project", "bobby's ai", "Anouncements"];
+const list = ["tranquiz", "lorem", "ipsunm"];
 let choice = list[Math.floor(Math.random(list) * list.length)];
 const heading = document.getElementById("lorem");
 heading.textContent = choice;   
 console.log(choice);
 
-const links = ["index.html", "/alpha/index.html", "/projects/aztecproject/aztec.html"];
-
 const anchor = document.getElementById("randomlink");
 
-if (choice=="khaled's aztec project"){
-  anchor.href=("https://port5.ie/projects/aztecproject/aztec");
+if (choice=="index"){
+  anchor.href=("https://");
 }
-else if (choice=="bobby's ai"){
-  anchor.href=("https://port5.ie/alpha/");
+else if (choice=="index"){
+  anchor.href=("https://");
 }
-else if (choice=="Anouncements"){
-  anchor.href=("https://port5.ie/projects/Anouncement");
+else if (choice=="index"){
+  anchor.href=("https://");
 }
